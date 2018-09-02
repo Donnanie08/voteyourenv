3cf8e67276f3111500e015734c825c58a906f13b
Xin Nie <donnanie08@gmail.com>
2018-09-02 05:29:22 -0400

    Added color ideas and design for front end

--- public/result.js
+++ public/result.js
@@ -1,3 +1,7 @@
+
+
+
+
 fetch('http://localhost:3000/poll').then(res => res.json())
 .then(data => {
     const votes = data.votes;
@@ -111,4 +115,150 @@ fetch('http://localhost:3000/poll2').then(res => res.json())
       addCloud();
     }
 
+});
+
+
+//implement real time charts on index.html
+fetch('http://localhost:3000/poll').then(res => res.json())
+.then(data => {
+    const votes = data.votes;
+    const totalVotes = votes.length;
+
+    //count vote points for each one - accumulator and current value
+    const voteCounts = votes.reduce((acc, vote) => (
+        (acc[vote.env] = (acc[vote.env] || 0) + parseInt(vote.points)), acc),
+        {}
+    );
+
+    let dataPoints = [
+        {label: 'Kindle', y: voteCounts.Kindle},
+        {label: 'Paper', y: voteCounts.Paper}
+    ];
+    
+    const chartContainer = document.querySelector('#chartContainer');
+    
+    if(chartContainer){
+        const chart = new CanvasJS.Chart('chartContainer', {
+            
+
+            animationEnabled: true,
+            
+            backgroundColor: "transparent",
+            colorSet: "greenShades",
+            axisY:{
+                title: "",
+                tickLength: 0,
+                lineThickness:0,
+                margin:0,
+                valueFormatString:" " //comment this to show numeric values
+            },
+            title: {
+                text: `Total Votes: ${totalVotes}`,
+                fontColor: "#364E4F",
+                fontSize: 30
+            },
+            data: [
+                {
+                    type: 'column',
+                    dataPoints: dataPoints,
+                    indexLabel: "{y}"
+                }
+            ]
+        });
+        chart.render();
+
+        //add liveview here
+    
+        //implement Pusher
+        // Enable pusher logging - don't include this in production
+        Pusher.logToConsole = true;
+    
+        var pusher = new Pusher('d3992811c04d6474365b', {
+          cluster: 'us2',
+          encrypted: true
+        });
+    
+        var channel = pusher.subscribe('env-poll');
+        channel.bind('env-vote', function(data) {
+            //add data to chart
+            dataPoints = dataPoints.map(x => {
+                if(x.label == data.env){
+                    x.y += data.points;
+                    return x;
+                }else{
+                    return x;
+                }
+            });
+            //re-render the chart to update
+            chart.render();
+        });
+    }
+});
+
+fetch('http://localhost:3000/poll2').then(res => res.json())
+.then(data => {
+    // console.log(data);
+    const votes = data.votes;
+
+    const totalVotes = votes.length;
+    // //Count vote for each one
+    const voteCounts = votes.reduce((acc, vote) => (
+        (acc[vote.vehicle] = (acc[vote.vehicle] || 0) + parseInt(vote.points)), acc),
+        {}
+    );
+
+    let dataPoints = [
+        {label: 'Car', y: voteCounts.Car},
+        {label: 'Bike', y: voteCounts.Bike}
+    ];
+    
+    const chartContainer = document.querySelector('#chartContainer2');
+    
+    if(chartContainer){
+        CanvasJS.addColorSet("greenShades",
+                [//colorSet Array
+                "#2F4F4F",
+                "#008080"               
+                ]);
+
+        const chart = new CanvasJS.Chart('chartContainer2', {
+            animationEnabled: true,
+            theme: 'light1',
+            backgroundColor: "transparent",
+            colorSet: "greenShades",
+            title: {
+                text: `Total Vehicle Votes: ${totalVotes}`
+            },
+            data: [
+                {
+                    type: 'column',
+                    dataPoints: dataPoints,
+                    indexLabel: "{y}"
+                }
+            ]
+        });
+        chart.render();
+    
+        Pusher.logToConsole = true;
+    
+        var pusher = new Pusher('5df99ea18c802431eb03', {
+          cluster: 'us2',
+          forceTLS: true
+        });
+    
+        var channel = pusher.subscribe('vehicle-poll');
+        channel.bind('vehicle-vote', function(data) {
+            //add data to chart
+            dataPoints = dataPoints.map(x => {
+                if(x.label == data.vehicle){
+                    x.y += data.points;
+                    return x;
+                }else{
+                    return x;
+                }
+            });
+    
+            chart.render();
+        });
+    }
 });
